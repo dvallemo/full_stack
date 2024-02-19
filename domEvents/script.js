e3bf575c764eef8c4c4a24a004a4f210b1655daf
dvallemo <davidvalle421@gmail.com>
2024-02-19 17:31:39 -0500

    complete shopping list with some repetititions

--- domEvents/script.js
+++ domEvents/script.js
@@ -5,7 +5,7 @@ var ul = document.querySelector("ul");
 //adding event listener
 button.addEventListener("click", function() { //button event listener
     if (input.value.length > 0) { //only if the length of the input.value is greater than zero
-        var li = document.createElement("li"); //grabs the li element and saves into a variable 
+        const li = document.createElement("li"); //grabs the li element and saves into a variable 
         li.appendChild(document.createTextNode(input.value)); //input.value gets the value enterd in the textbox
         ul.appendChild(li); //appends li child into ul parent
         input.value = ""; //after pressing enter the text box resets to an empty string
@@ -13,12 +13,13 @@ button.addEventListener("click", function() { //button event listener
 })
 
 //adding key press event: use javascript character codes: enter is key code 13
-input.addEventListener("keypress", function() { //"keydown" 
-    console.log(event.which);
-//     if (input.value.length > 0) { 
-//         var li = document.createElement("li"); 
-//         li.appendChild(document.createTextNode(input.value)); 
-//         ul.appendChild(li); 
-//         input.value = ""; 
-//     }
+input.addEventListener("keydown", function(event) {  //input is event becyase keypress requires it
+    if (input.value.length > 0 && event.key === "Enter") { //code only runs if the legth of the textNode child of li is greater than zero and the event key is 13 or if the user presses the button enter/return
+        var li = document.createElement("li"); 
+        li.appendChild(document.createTextNode(input.value)); 
+        ul.appendChild(li); 
+        input.value = ""; 
+    }
 })
+
+//changed event.keyCode === 13 to even.key === "Enter" becuase event.keyCode ===13 got depreciated
